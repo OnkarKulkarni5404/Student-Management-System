dabbf7a1b1d7a997243459d0235af49b82b49916
Onkar Kulkarni <onkar_kulkarni@persistent.co.in>
2019-11-18 16:53:13 +0530

    Added Basic Frame for Token

---
+++ token/src/main/java/com/token/TokenApplication.java
@@ -0,0 +1,15 @@
+package com.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenApplication.class, args);
+	}
+
+}
